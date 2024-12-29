44d4060c79202459ce038859da197c31fc4f9966
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-29 16:50:58 +0700

    Membuat fungsi getMhs

--- app/src/main/java/com/example/prak9db/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/prak9db/ui/home/viewmodel/HomeViewModel.kt
@@ -1,9 +1,16 @@
 package com.example.prak9db.ui.home.viewmodel
 
+
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.prak9db.model.Mahasiswa
 import com.example.prak9db.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class HomeUiState{
     data class Succes(val mahasiswa: List<Mahasiswa>):HomeUiState()
@@ -18,4 +25,21 @@ class HomeViewModel (private val mhs: MahasiswaRepository): ViewModel(){
     init {
         getMhs()
     }
+
+
+    fun getMhs(){
+        viewModelScope.launch{
+            mhsUIState = HomeUiState.Loading
+            mhsUIState=try{
+                HomeUiState.Succes(mhs.getMahasiswa())
+            }catch (e: IOException){
+                HomeUiState.Error
+            }catch(e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
+
+
 }
+
