4b746d5892ee6b43315c6d344e1f1fde7815d7a9
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-30 05:49:30 +0700

    mendefinisikan sebuah destinasi home

---
+++ app/src/main/java/com/example/prak9db/ui/home/screen/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.prak9db.ui.home.screen
+
+import com.example.prak9db.navigation.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route= "home"
+    override val titleRes= "Home Mhs"
+}
