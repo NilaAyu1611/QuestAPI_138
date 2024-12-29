038836272689d5d3ca6c881ac52193ac176d9ef8
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-29 15:41:23 +0700

    Membuat fungsi InsertUiEvent untuk tabel Mahasiswa

--- app/src/main/java/com/example/prak9db/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/prak9db/ui/home/viewmodel/InsertViewModel.kt
@@ -2,6 +2,7 @@ package com.example.prak9db.ui.home.viewmodel
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.prak9db.model.Mahasiswa
 import com.example.prak9db.repository.MahasiswaRepository
 
 
@@ -16,4 +17,23 @@ class InsertViewModel (private mhs: MahasiswaRepository): ViewModel(){
 
 data class InsertUiState(
     val insertUiEvent:InsertUiEvent = InsertUiEvent()
+)
+
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
 )
