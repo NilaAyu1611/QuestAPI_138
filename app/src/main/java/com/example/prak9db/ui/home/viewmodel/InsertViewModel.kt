e09599c9d1acc4531774228a16ba16f4a7c9bea2
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-29 15:32:09 +0700

    Membuat class InsertViewModel

---
+++ app/src/main/java/com/example/prak9db/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.prak9db.ui.home.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.prak9db.repository.MahasiswaRepository
+
+
+class InsertViewModel (private mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+
+}
