245b44829bc73afa41d531cf17c23238ac8f60f1
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-30 05:07:28 +0700

    Mendifinisikan detail sebuah destinasi navigasi

---
+++ app/src/main/java/com/example/prak9db/ui/home/screen/InsertView.kt
@@ -0,0 +1,9 @@
+package com.example.prak9db.ui.home.screen
+
+import com.example.prak9db.navigation.DestinasiNavigasi
+
+object Destinasi:DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
+
