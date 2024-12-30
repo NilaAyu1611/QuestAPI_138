a69fe05ccd8f7cf584c5325df756443da769d2d1
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-30 13:14:28 +0700

    typo MahasiswaApp

--- app/src/main/java/com/example/prak9db/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak9db/ui/PenyediaViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.prak9db.ui.home.viewmodel
+package com.example.prak9db.ui
 
 
 import androidx.lifecycle.ViewModelProvider
@@ -6,13 +6,15 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak9db.MahasiswaApplications
+import com.example.prak9db.ui.home.viewmodel.HomeViewModel
+import com.example.prak9db.ui.home.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiKontak().container.mahasiswaRepository)}
-        initializer { InsertViewModel(aplikasiKontak().container.mahasiswaRepository)}
+        initializer { HomeViewModel(MahasiswaApp().container.mahasiswaRepository) }
+        initializer { InsertViewModel(MahasiswaApp().container.mahasiswaRepository) }
     }
 }
 
-fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+fun CreationExtras.MahasiswaApp(): MahasiswaApplications =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
