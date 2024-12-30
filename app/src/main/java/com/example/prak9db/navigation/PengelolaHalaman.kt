dba46ae93a7151c6321ada48a5b981625c47f275
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-30 08:20:00 +0700

    Membat fungsi untuk mengelola halaman

---
+++ app/src/main/java/com/example/prak9db/navigation/PengelolaHalaman.kt
@@ -0,0 +1,40 @@
+package com.example.prak9db.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.prak9db.ui.home.screen.DestinasiEntry
+import com.example.prak9db.ui.home.screen.DestinasiHome
+import com.example.prak9db.ui.home.screen.EntryMhsScreen
+import com.example.prak9db.ui.home.screen.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost (
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive= true
+                    }
+                }
+            })
+        }
+    }
+}
